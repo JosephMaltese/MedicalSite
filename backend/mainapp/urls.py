240e3198b443236c60e987ee3ca82fea4dfefa72
JosephMaltese <josephmaltese34@gmail.com>
2025-07-01 01:01:20 -0400

    Initial commit

---
+++ backend/mainapp/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+# from .views import PostViewSet
+from . import views
+
+urlpatterns = [
+    path('', views.getUsers),
+    path('create', views.addUser),
+    path('read/<str:pk>', views.getUser),
+    path('update/<str:pk>', views.updateUser),
+    path('delete/<str:pk>', views.deleteUser),
+]
