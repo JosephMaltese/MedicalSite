ca6ed0c1fff0baee0f39b51353095ad68e6a67f1
SpeakBoy <nevinspeakman@outlook.com>
2025-07-14 20:34:22 -0400

    Add tailwind_v4, create basic landing page

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import NavBar from './components/NavBar.vue';
+import LoginBox from './components/LoginBox.vue';
 import { onMounted } from 'vue';
 
 onMounted(() => {
@@ -8,16 +9,8 @@ onMounted(() => {
 </script>
 
 <template>
-  <div>
-    <p class="bg-green-400">hi</p>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <NavBar></NavBar>
+  <LoginBox></LoginBox>
 </template>
 
 <style scoped>
---
+++ frontend/src/components/NavBar.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div class="navbar bg-primary shadow-sm">
+    <div class="navbar-start">
+      <a class="btn btn-ghost hover:text-primary text-xl text-primary-content text-shadow-sm text-shadow-neutral" href=".">Medical Site</a>
+    </div>
+    <div class="navbar-center">
+    </div>
+    <div class="navbar-end"></div>
+  </div>
+</template>
