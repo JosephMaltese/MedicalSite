af1151d89561bdf0d3b4198afce3171798611c17
JosephMaltese <josephmaltese34@gmail.com>
2025-07-17 19:01:13 -0400

    Fixed navbar

--- frontend/src/components/Navbar.vue
+++ frontend/src/components/Navbar.vue
@@ -1,9 +1,13 @@
-<script setup>
-
+<script setup lang="ts">
 </script>
 
 <template>
-    <p class="text-red-500">
-        navbar placeholder
-    </p>
+  <div class="navbar bg-primary shadow-sm">
+    <div class="navbar-start">
+      <a class="btn btn-ghost hover:text-primary text-xl text-primary-content text-shadow-sm text-shadow-neutral" href=".">Medical Site</a>
+    </div>
+    <div class="navbar-center">
+    </div>
+    <div class="navbar-end"></div>
+  </div>
 </template>
