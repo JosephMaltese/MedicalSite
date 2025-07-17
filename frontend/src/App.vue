80fdc81370350de8c179cf8595487585a0f04cd0
JosephMaltese <josephmaltese34@gmail.com>
2025-07-17 19:43:51 -0400

    Fixed rendering App via RouterView

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,29 +1,15 @@
 <script setup lang="ts">
-import NavBar from './components/NavBar.vue';
-import LoginBox from './components/LoginBox.vue';
-import { onMounted } from 'vue';
+import Navbar from './components/Navbar.vue';
+import { RouterView } from 'vue-router';
+// import { onMounted } from 'vue';
 
-onMounted(() => {
-  fetch(`${import.meta.env.VITE_API_URL}users`).then(res => res.json()).then(data => {console.log(data)}).catch(error => {console.error(error)})
-})
+// onMounted(() => {
+//   fetch(`${import.meta.env.VITE_API_URL}users`).then(res => res.json()).then(data => {console.log(data)}).catch(error => {console.error(error)})
+// })
 </script>
 
 <template>
-  <NavBar></NavBar>
-  <LoginBox></LoginBox>
+  <Navbar />
+  <RouterView />
+  
 </template>
-
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router';
-import HomeView from '@/views/HomeView.vue';
+import HomeView from '../views/HomeView.vue';
+import LoginView from '../views/LoginView.vue';
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
     routes: [
@@ -8,6 +9,11 @@ const router = createRouter({
             name: 'home',
             component: HomeView
 
+        },
+        {
+            path: '/login',
+            name: 'login',
+            component: LoginView
         }
     ]
 })
